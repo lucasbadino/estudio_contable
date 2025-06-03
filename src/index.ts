05211aacab432f1cc3d5dca0846a1a315687326a
lucas <lucas.badino.7@gmail.com>
2025-06-03 16:29:21 -0300

    fix some problems

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import 'reflect-metadata';
 import express from 'express';
 import { AppDataSource } from './config/data-source';
 import routerClients from './rutes/client.routes';
+import morgan from 'morgan';
+
 
 const app = express();
 app.use(express.json());
+app.use(morgan('short'));
 app.use(routerClients);
 
 AppDataSource.initialize()
